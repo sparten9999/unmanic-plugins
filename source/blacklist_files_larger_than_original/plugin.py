7ff401f34c8dd5ce0afd2974e8dab1a2d4f1f124
sparten9999 <kissrock247@aol.com>
2022-01-23 16:10:29 -0500

    added spdx header?

--- source/blacklist_files_larger_than_original/plugin.py
+++ source/blacklist_files_larger_than_original/plugin.py
@@ -28,14 +28,18 @@
 
 
 """
-Copyright (C) <2022> <sparten9999>
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
-
+    Written by:               sparten9999
+    Date:                     23 Jan 2022, (04:10 PM)
+ 
+    Copyright:
+        Copyright (C) 2021 Josh Sunnex
+        This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+        Public License as published by the Free Software Foundation, version 3.
+        This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+        implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+        for more details.
+        You should have received a copy of the GNU General Public License along with this program.
+        If not, see <https://www.gnu.org/licenses/>.
 """
 
 
